4fa5f3cdce5c700433a60642cfbf5cc3b882cfb4
Sebastian Kasanzew <sebkasanzew@gmail.com>
2017-04-23 23:33:37 +0200

    Add yarn packaging
    add stylesheet for impress.js adjustments

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import 'impress.js';
 
 /* global impress:true */
 
+require("style-loader?modules!./index.css");
+
 impress().init();
--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,24 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-    context: path.resolve(__dirname, './src'),
-    entry: {
-        main: './index.js',
-        vendor: ['impress.js']
-    },
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: '[name].bundle.js',
-    },
-    devServer: {
-        contentBase: path.resolve(__dirname, './src')
-    },
-}
+  context: path.resolve(__dirname, './src'),
+  entry: {
+    main: './index.js',
+    vendor: ['impress.js']
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader",
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, './src')
+  },
+};
